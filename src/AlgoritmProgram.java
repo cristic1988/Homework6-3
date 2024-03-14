8309ab18e147c72fd906d2572937f33b4ba2ef39
cristic1988 <cristic8@gmail.com>
2024-03-14 15:44:02 +0200

    first commit

---
+++ src/AlgoritmProgram.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class AlgoritmProgram {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int intNum = random.nextInt(100_011)+11;
+        System.out.println(intNum);
+
+
+
+
+    }
+
+}
+
