d872e70b1ce44257c93f999fff4df6c66ef377ff
cristic1988 <cristic8@gmail.com>
2024-03-14 23:26:33 +0200

    second commit

--- src/AlgoritmProgram.java
+++ src/AlgoritmProgram.java
@@ -1,17 +1,27 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class AlgoritmProgram {
 
+
     public static void main(String[] args) {
         Random random = new Random();
 
-        int intNum = random.nextInt(100_011)+11;
-        System.out.println(intNum);
+        int intNum = random.nextInt(100_011) + 11;
+        System.out.print("Сумма числа " + intNum + ": ");
 
 
+        int sum = 0;
+        while (intNum > 0) {
+            sum += intNum % 10;
+            intNum /= 10;
 
+        }
+        System.out.print(sum);
 
     }
-
 }
+
+
+
 
